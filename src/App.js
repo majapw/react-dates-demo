3fae0c1808b0bf158ce937b687cc516d711a4670
Maja Wichrowska <maja.wichrowska@airbnb.com>
2017-10-09 11:28:03 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import 'react-dates/initialize';
+import 'react-dates/lib/css/_datepicker.css';
+
+import { DateRangePicker } from 'react-dates';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: null,
+      endDate: null,
+      focusedInput: null,
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <DateRangePicker
+          startDate={this.state.startDate}
+          endDate={this.state.endDate}
+          onDatesChange={({ startDate, endDate }) => { this.setState({ startDate, endDate })}}
+          focusedInput={this.state.focusedInput}
+          onFocusChange={(focusedInput) => { this.setState({ focusedInput })}}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
